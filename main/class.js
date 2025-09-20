a259c1031003fbd4794c8e77b6f9acabe57241cf
Dingyijie <cheritasee@gmail.com>
2025-09-20 08:59:50 -0400

    1

--- main/class.js
+++ main/class.js
@@ -1,26 +1,27 @@
 function downloadFile(filename, content, type="text/plain") {
-      const blob = new Blob([content], { type });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = url;
-      a.download = filename;
-      a.click();
-      URL.revokeObjectURL(url);
-    }
+    const blob = new Blob([content], { type });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    a.click();
+    URL.revokeObjectURL(url);
+}
 
-
+document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("download-html").onclick = () => {
-      fetch("index.html") 
-        .then(r => r.text())
-        .then(text => {
-          downloadFile("my-page.html", text, "text/html");
-        });
+        fetch("index.html") 
+            .then(r => r.text())
+            .then(text => {
+                downloadFile("my-page.html", text, "text/html");
+            });
     };
 
     document.getElementById("download-css").onclick = () => {
-      fetch("class.css")
-        .then(r => r.text())
-        .then(text => {
-          downloadFile("my-style.css", text, "text/css");
-        });
+        fetch("class.css")
+            .then(r => r.text())
+            .then(text => {
+                downloadFile("my-style.css", text, "text/css");
+            });
     };
+});
