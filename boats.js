a67a7cde31733c4c8088437f5b63389550868297
Dingyijie <cheritasee@gmail.com>
2025-10-01 19:25:02 +0000

    2

---
+++ boats.js
@@ -0,0 +1,24 @@
+unction downloadFile(filename, content, type="text/plain") {
+    const blob = new Blob([content], { type });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("download-all").onclick = () => {
+        fetch("boats.html") 
+            .then(r => r.text())
+            .then(text => {
+                downloadFile("my-page.html", text, "text/html");
+            });
+        fetch("event_boats.css")
+            .then(r => r.text())
+            .then(text => {
+                downloadFile("my-style.css", text, "text/css");
+            });
+    };
+});
