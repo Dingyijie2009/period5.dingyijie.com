2360038e5a999b37e102c8c9bc476e671436e294
Dingyijie <cheritasee@gmail.com>
2025-10-02 18:48:41 +0000

    1

---
+++ main/events.js
@@ -0,0 +1,24 @@
+function downloadFile(filename, content, type="text/plain") {
+    const blob = new Blob([content], { type });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("download-all").onclick = () => {
+        fetch("events.html") 
+            .then(r => r.text())
+            .then(text => {
+                downloadFile("my-page.html", text, "text/html");
+            });
+        fetch("events.css")
+            .then(r => r.text())
+            .then(text => {
+                downloadFile("my-style.css", text, "text/css");
+            });
+    };
+});
